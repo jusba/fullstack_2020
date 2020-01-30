fbcfecff0bd3b00b1803de51559d2383c028dcc9
jusba <36404014+jusba@users.noreply.github.com>
2020-01-30 21:26:16 +0200

    2.5 rdy

---
+++ Osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+
+
+const Header = ({ course }) => {
+
+    return (
+
+
+
+        <h3>{course}</h3>
+
+    )
+}
+const Part = (props) => {
+    return (
+        <>{props.osa1} {props.osa11}</>
+    )
+}
+const Content = (props) => {
+
+
+    return (
+
+        <>
+            {props.course.parts.map(course =>
+                <p key={course.id}>
+                    <Part osa1={course.name} osa11={course.exercises} osaId={course.id} />
+                </p>
+            )}
+        </>
+    )
+
+
+
+}
+const Total = (course) => {
+
+    return (
+
+        <b>Total of&nbsp;
+
+            {course.course.parts.reduce(
+            (sum, course) => sum = sum + course.exercises, 0)}
+            &nbsp;exercises
+        </b>
+
+    )
+}
+const Course = (course) => {
+
+    return (
+        <div>
+            <Header course={course.course.name} />
+            <Content course={course.course} />
+            <Total course={course.course} />
+        </div>
+
+    )
+
+}
+export default Course
--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -1,63 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = ({ course }) => {
-
-    return (
+import Course from "./components/Course"
 
 
-        <>
-            <h3>{course}</h3>
-        </>
-    )
-}
-const Part = (props) => {
-    return (
-        <>{props.osa1} {props.osa11}</>
-    )
-}
-const Content = (props) => {
-
-
-    return (
-
-        <>
-            {props.course.parts.map(course =>
-                <p key={course.id}>
-                    <Part osa1={course.name} osa11={course.exercises} osaId={course.id} />
-                </p>
-            )}
-        </>
-    )
-
-
-
-}
-const Total = (course) => {
-
-    return (
-
-        <b>Total of&nbsp;
-                
-            {course.course.parts.reduce(
-                (sum, course) => sum = sum + course.exercises, 0)}
-            &nbsp;exercises
-        </b>
-
-    )
-}
-const Course = (course) => {
-
-    return (
-        <div>
-            <Header course={course.course.name} />
-            <Content course={course.course} />
-            <Total course={course.course} />
-        </div>
-
-    )
-
-}
 
 
 const App = () => {
@@ -107,13 +52,16 @@ const App = () => {
     ]
 
     return (
-        courses.map(course =>
-            <div key = {course.id}>
-                <Course course={course} />
-            </div>
-        
-        )
-        
+        <div>
+            <h1>Web development curriculum</h1>
+            {courses.map(course =>
+                <div key={course.id}>
+                    <Course course={course} />
+                </div>
+
+            )}
+        </div>
+
     )
 }
 
