47f2df2a2b954583a05c3f2477fa19e55eda45f4
jusba <36404014+jusba@users.noreply.github.com>
2020-02-17 13:23:31 +0200

    4.18 done

--- Osa4/blogilista/app.js
+++ Osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
 
@@ -15,6 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const errorHandler = (error, request, response, next) => {
 
